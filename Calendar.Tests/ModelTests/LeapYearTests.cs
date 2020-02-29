50ef347e28b5426fe937b0211dea9227c244acc6
Andrew Philpott <andrewphilpott92@gmail.com>
2020-02-29 15:17:18 -0800

    Add test to see if year is divisible by four.

--- Calendar.Tests/ModelTests/LeapYearTests.cs
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -9,6 +9,8 @@ namespace Calendar.Tests
     [TestMethod]
     public void IsLeapYear_NumberDivisibleByFour_True()
     {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
     }
   }
 }
